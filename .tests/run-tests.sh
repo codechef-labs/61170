97515d2aa62f44c2e0242319a824e1dc507653f3
Aniruddh Dubey <justani98@gmail.com>
2024-11-29 15:47:47 +0530

    Add template

--- .tests/run-tests.sh
+++ .tests/run-tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # cd /home/chef/workspace
+npm install -D vitest @testing-library/react @testing-library/jest-dom jsdom
+
 node node_modules/vitest/vitest.mjs run --config=.tests/config.js --reporter=json --outputFile=.tests/payload.json
+
 node .tests/process.js
